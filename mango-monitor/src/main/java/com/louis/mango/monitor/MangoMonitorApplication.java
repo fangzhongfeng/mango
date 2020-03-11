acbf16df796079bf3730b0b9911a81d8dac898a8
fangzhongfeng <64496419@qq.com>
2020-03-11 23:07:02 +0800

    单机版eureka-server.client 配置

--- mango-monitor/src/main/java/com/louis/mango/monitor/MangoMonitorApplication.java
+++ mango-monitor/src/main/java/com/louis/mango/monitor/MangoMonitorApplication.java
@@ -4,13 +4,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * 启动器
  * @author Louis
  * @date Jan 15, 2019
  */
-@EnableAdminServer
+
+@EnableDiscoveryClient
 @SpringBootApplication
 public class MangoMonitorApplication {
 
