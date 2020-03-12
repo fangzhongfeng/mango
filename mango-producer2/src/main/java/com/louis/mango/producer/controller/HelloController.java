607efeaa1c171b76c22ce64602d1471df56c66da
fangzhongfeng <64496419@qq.com>
2020-03-12 22:32:59 +0800

    配置

--- mango-producer2/src/main/java/com/louis/mango/producer/controller/HelloController.java
+++ mango-producer2/src/main/java/com/louis/mango/producer/controller/HelloController.java
@@ -1,5 +1,6 @@
 package com.louis.mango.producer.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,9 +13,14 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 @ResponseBody
 public class HelloController {
+
+    @Value("${server.port}")
+    private String port;
+
+
     @RequestMapping("/hello")
     public  String hello(){
-        return "hello  world";
+        return "hello  world"+port;
     }
 
 }
