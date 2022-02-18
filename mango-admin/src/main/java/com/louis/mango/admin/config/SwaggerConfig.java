9a0d83d5ec4b37baa6ea616e686c028d3cebf4ef
java-fans <64496419@qq.com>
2022-02-18 22:59:41 +0800

    test 合并代码时候  远程仓库一家有人提交

--- mango-admin/src/main/java/com/louis/mango/admin/config/SwaggerConfig.java
+++ mango-admin/src/main/java/com/louis/mango/admin/config/SwaggerConfig.java
@@ -15,9 +15,7 @@ import springfox.documentation.service.Parameter;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
+ 
 
 /**
  * Swagger配置
